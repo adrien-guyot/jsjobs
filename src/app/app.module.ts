9ff4c8497cbbd7c38d2ea60bbb1aa91adabe0eb9
mookylicious <adrienguyot17@gmail.com>
2017-09-04 12:02:00 +0200

    Création de formulaire avec reactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { JobService } from './services/job.service';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { JobListComponent } from './job-list/job-list.component';
+import { JobAddFormComponent } from './job-add-form/job-add-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    JobListComponent
+    JobListComponent,
+    JobAddFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [JobService],
