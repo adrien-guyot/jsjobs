8705b2c785976679287fc570bbb1cf8ce881b076
mookylicious <adrienguyot17@gmail.com>
2017-09-13 11:01:56 +0200

    Ajout des boutons radios pour les déplacements

--- src/app/job-add-form/job-add-form.component.ts
+++ src/app/job-add-form/job-add-form.component.ts
@@ -10,6 +10,39 @@ export class JobAddFormComponent implements OnInit {
 
   form: FormGroup;
 
+  contractTypes = [
+    { id: 1, name: 'Stage', value: 'intership' },
+    { id: 2, name: 'Interim', value: 'temp' },
+    { id: 3, name: 'Contrat à durée déterminée (CDD)', value: 'fixed-term' },
+    { id: 4, name: 'Contrat à durée indéterminée (CDI)', value: 'permanent' },
+    { id: 5, name: 'Independant', value: 'freelance' }
+  ];
+
+  currencies = [
+    { id: 1, name: 'euros', value: 'EU', symbol: '€' },
+    { id: 2, name: 'livres sterling', value: 'POUNDS', symbol: '£' },
+    { id: 3, name: 'francs CFA', value: 'CFA', symbol: 'CFA' },
+    { id: 4, name: 'dollars canadien', value: 'CAD', symbol: '$' }
+  ];
+
+  statuses = [
+    { id: 1, name: 'cadre', value: 'executive' },
+    { id: 1, name: 'employé', value: 'employee' }
+  ];
+
+  experience = [
+    { id: 1, name: 'junior', value: 'junior' },
+    { id: 2, name: 'medior', value: 'medior' },
+    { id: 3, name: 'senior', value: 'senior' }
+  ];
+
+  areas = [
+    { id: 1, name: 'aucun déplacements', value: 'none' },
+    { id: 2, name: 'déplacements régionaux', value: 'region' },
+    { id: 3, name: 'déplacements nationaux', value: 'nation' },
+    { id: 4, name: 'déplacements internationaux', value: 'international' }
+  ];
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -21,8 +54,8 @@ export class JobAddFormComponent implements OnInit {
       zipcode: 35,
       decription: '',
       contract: '',
-      salary: 0,
-      currency: 'euros',
+      salary: null,
+      currency: '',
       startdate: new Date(),
       experience: '',
       status: '',
@@ -33,7 +66,7 @@ export class JobAddFormComponent implements OnInit {
     });
   }
 
-  createJob(){
+  createJob() {
     console.log(this.form.value);
   }
 
