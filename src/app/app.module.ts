c727e1ec51525e898ac8fef51f34dd68735faf83
mookylicious <adrienguyot17@gmail.com>
2017-10-03 11:15:13 +0200

    Création du pipe truncate pour tronquer les descriptions de jobs trop longues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { SearchResultComponent } from './search-result/search-result.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 const routes = [
   { path: '', component: HomeComponent },
@@ -50,7 +51,8 @@ const routes = [
     SearchResultComponent,
     AuthenticationComponent,
     RegisterComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/truncate.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate'
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: any, limit = 15, end = '...'): any {
+    // "aze aze aze aze aze aze" | truncate : 5
+    let shortenedValue = '';
+    if (value) {
+      let words = value.split(/\s+/);
+      if (words.length > limit) {
+        shortenedValue = words.slice(0, limit).join(' ') + end;
+      }else{
+        shortenedValue = value;
+      }
+    }
+    return shortenedValue;
+  }
+
+}
